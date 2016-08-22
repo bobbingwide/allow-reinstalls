50ab9319172612e8147e3cbc7d770be0f8bdc631
Herb Miller <herb@bobbingwide.com>
2016-08-22 23:20:43 +0100

    Fixes #1 Add PHPUnit testing and test with WordPress 4.6

---
+++ tests/test-allow-reinstalls.php
@@ -0,0 +1,26 @@
+<?php // (C) Copyright Bobbing Wide 2016
+/**
+ * @group allow-reinstalls
+ */
+class Tests_allow_reinstalls extends WP_UnitTestCase {
+
+	/**
+	 * Confirm which hook will be invoked
+	 * 
+	 * In the absence of a function to confirm that the filter has been added
+	 * we just check that the filter function exists.
+	 * If it doesn't then it probably means the plugin is not activated.
+	 *
+	 */
+	function test_allow_reinstalls_hooked_in() {
+		$this->assertTrue( function_exists( "allow_reinstalls_upgrader_package_options" ) );
+	}
+
+	function test_upgrader_package_options() {
+		$options = array();
+		$options['abort_if_destination_exists'] = true;
+		$options = apply_filters( "upgrader_package_options", $options );
+		$this->assertFalse( $options['abort_if_destination_exists'] );
+	}
+}
+
