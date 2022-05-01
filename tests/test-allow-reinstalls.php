5daa46f002dbfaea2bae197b3b761573fd0465aa
Herb Miller <herb@bobbingwide.com>
2022-05-01 10:01:09 +0100

    Update to show plugin tested with WordPress 5.9.3

--- tests/test-allow-reinstalls.php
+++ tests/test-allow-reinstalls.php
@@ -1,9 +1,15 @@
-<?php // (C) Copyright Bobbing Wide 2016
+<?php // (C) Copyright Bobbing Wide 2016-2021
 /**
  * @group allow-reinstalls
  */
 class Tests_allow_reinstalls extends BW_UnitTestCase {
-
+	
+	function test_allow_reinstalls_plugin_active() {
+		oik_require( 'admin/oik-activation.php', 'oik-batch' );
+		$active = oik_plugin_is_plugin_activated( 'allow-reinstalls' );
+		$this->assertTrue( $active, "Please activate allow-reinstalls to test." );
+		
+	}	
 	/**
 	 * Confirm which hook will be invoked
 	 * 
@@ -22,5 +28,8 @@ class Tests_allow_reinstalls extends BW_UnitTestCase {
 		$options = apply_filters( "upgrader_package_options", $options );
 		$this->assertFalse( $options['abort_if_destination_exists'] );
 	}
+	
+	
+	
+	
 }
-
